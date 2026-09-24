e6f393d3b3eaf369ae1f9b5ad7bf013a642d0879
raghav0123 <sundriyalraghav123@gmail.com>
2026-09-24 20:41:32 +0530

    unit test added!

---
+++ backend/test/validateTodo.test.js
@@ -0,0 +1,50 @@
+const validateTodo = require('../middlewares/validateTodo')
+
+describe('ValidateTodo middleware', () => {
+    let req,res,next;
+    beforeEach(() => {
+        req = {
+            params: {}
+        }
+        res = {
+            status: jest.fn().mockReturnThis(200),
+            json: jest.fn()
+        }
+        next = jest.fn()
+        jest.clearAllMocks();
+    })
+
+    test('should return 400 if name is missing', async () => {
+        req = {body: {email:'a@gmail.com'}}
+        await validateTodo(req,res,next)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({
+            success: false,
+            message: "Name is required!"
+        })
+    })
+
+    test('should return 400 if description is missing', async () => {
+        req = {body: {name:'a@gmail.com'}}
+        await validateTodo(req,res,next)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({
+            success: false,
+            message: "Description is required!"
+        })
+    })
+
+    test('should return 200 and call next() if all perfect', async () => {
+        req = {body: 
+            {name:'a@gmail.com'
+            ,description:'r@gmail.com'
+        }
+    }
+        await validateTodo(req,res,next)
+       expect(res.status).not.toHaveBeenCalled()
+        expect(res.json).not.toHaveBeenCalled()
+       
+        expect(next).toHaveBeenCalled();
+    })
+
+})
