55fae56db3f65dc31fd04fb5dec8b17a79960cbe
raghav0123 <sundriyalraghav123@gmail.com>
2026-09-25 02:20:40 +0530

    Add CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 require('dotenv').config(); 
 const todoRoutes = require('./routes/todoRoutes');
+import cors from "cors";
 
+app.use(cors());
 const app = express();
 
 app.use(express.json()); 
